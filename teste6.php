2ecd2d8342d1b4467679c469744ebc5e3f8a1c91
Bernardo Gelain Dariva <105089756+BernardoGelain@users.noreply.github.com>
2022-10-26 06:34:05 -0500

    Create teste6.php

---
+++ teste6.php
@@ -0,0 +1,29 @@
+<html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <br><br>
+    <form method="POST">
+    <label> Insira um valor
+        <input type="number" id = "numero" name="numero">
+    </label>
+    <input type="submit" value="enviar">
+    </form>
+    <?php
+    $numero = $_POST['numero'];
+    $calc = 1;
+    while ($numero > 1){
+        $calc *= $numero;
+        $numero--;
+    }
+
+    echo $calc;
+    ?>
+ </body>
+ </html>
+
+   
